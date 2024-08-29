12983db6eb52ce93c69c77c5ca2daa71ab4d0816
Nurdas <63863660+nurdas00@users.noreply.github.com>
2024-08-29 19:22:51 +0600

    test commit

--- src/main/java/kg/mega/petproject1/PetProject1Application.java
+++ src/main/java/kg/mega/petproject1/PetProject1Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PetProject1Application {
+	ты че тут пишешь
 фывафывафыафыва
 	фывафывафываыфвафывафыва
 	public static void main(String[] args) {
