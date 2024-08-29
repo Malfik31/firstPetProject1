1c2abf6d7e28e99920365fb45fcb47230f93f11b
nurdas00 <nurdas00@gmail.com>
2024-08-29 19:34:49 +0600

    test commit2

--- src/main/java/kg/mega/petproject1/PetProject1Application.java
+++ src/main/java/kg/mega/petproject1/PetProject1Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PetProject1Application {
 //test
+	//test2
 	public static void main(String[] args) {
 		SpringApplication.run(PetProject1Application.class, args);
 	}
