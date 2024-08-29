8ad48de51fd346eccd38bee4553f5dac831f9773
nurdas00 <nurdas00@gmail.com>
2024-08-29 19:24:32 +0600

    Revert "test commit"
    This reverts commit 12983db6eb52ce93c69c77c5ca2daa71ab4d0816.

--- src/main/java/kg/mega/petproject1/PetProject1Application.java
+++ src/main/java/kg/mega/petproject1/PetProject1Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PetProject1Application {
-	ты че тут пишешь
 фывафывафыафыва
 	фывафывафываыфвафывафыва
 	public static void main(String[] args) {
