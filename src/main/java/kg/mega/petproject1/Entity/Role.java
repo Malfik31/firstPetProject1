9da8047e9d5b637c2b58df44bcdf1ea941c598ee
Acer <malfik31@gmail.com>
2024-09-05 18:30:01 +0600

    create entity for role and add dependency for spring security

---
+++ src/main/java/kg/mega/petproject1/Entity/Role.java
@@ -0,0 +1,25 @@
+package kg.mega.petproject1.Entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+
+
+@Entity
+@Getter
+@Setter
+@Table(name = "role")
+public class Role implements GrantedAuthority {
+@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private int id;
+
+private String name;
+
+    @Override
+    public String getAuthority() {
+        return "";
+    }
+}
