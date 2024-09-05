636cde0f11c1426b3d611240c13fe202a5a75d6b
nurdas00 <nurdas00@gmail.com>
2024-09-05 19:49:51 +0600

    rename folder, delete redundant class

--- src/main/java/kg/mega/petproject1/entity/Role.java
+++ src/main/java/kg/mega/petproject1/entity/Role.java
@@ -1,10 +1,11 @@
 package kg.mega.petproject1.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.util.List;
 
 @Getter
 @Setter
@@ -14,10 +15,4 @@ public class Role {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    @ManyToMany
-    @JoinTable(name = "user_roles",
-            joinColumns = @JoinColumn(name = "role_id"),
-            inverseJoinColumns = @JoinColumn(name = "user_id"))
-    private List<User> users;
-
 }
