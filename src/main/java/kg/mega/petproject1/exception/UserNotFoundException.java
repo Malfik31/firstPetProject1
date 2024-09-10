7701eba7da300865370acc2259e9e0bd05d6fb92
Mariyam168 <maruusyaaa@gmail.com>
2024-09-10 19:29:07 +0600

    RealUserNotFoundException

--- src/main/java/kg/mega/petproject1/exception/UserNotFoundException.java
+++ src/main/java/kg/mega/petproject1/exception/UserNotFoundException.java
@@ -2,6 +2,6 @@ package kg.mega.petproject1.exception;
 
 public class UserNotFoundException extends Exception {
     public UserNotFoundException(String userId) {
-        super("User with id: " + userId+" not found");
+        super("User with id: " + userId + " not found");
     }
 }
