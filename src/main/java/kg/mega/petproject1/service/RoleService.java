1985f764a3cefae945fdd46cf61ec12da0851ffd
Acer <malfik31@gmail.com>
2024-09-10 18:53:11 +0600

    fix exception

--- src/main/java/kg/mega/petproject1/service/RoleService.java
+++ src/main/java/kg/mega/petproject1/service/RoleService.java
@@ -3,9 +3,12 @@ package kg.mega.petproject1.service;
 import kg.mega.petproject1.entity.Role;
 import kg.mega.petproject1.repository.RoleRepository;
 import lombok.AllArgsConstructor;
+import org.hibernate.ObjectNotFoundException;
 import org.springframework.stereotype.Service;
 
 
+import javax.management.relation.RoleNotFoundException;
+import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,11 +30,11 @@ public class RoleService {
         roleRepository.delete(role);
     }
 
-    public Role getById(int id) {
+    public Role getById(int id) throws FileNotFoundException {
         Optional<Role> role = roleRepository.findById((long) id);
         if (role.isPresent()) {
             return role.get();
-        } else throw new RuntimeException("Role not found");
+        } else throw new FileNotFoundException("role with this id not found in database");
     }
     //исправление
 }
