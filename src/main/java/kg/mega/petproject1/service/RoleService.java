f8f23e17335f3fb9fa22c656f3bc99ff1cddcbe8
nurdas00 <nurdas00@gmail.com>
2024-09-10 18:59:11 +0600

    fix review

--- src/main/java/kg/mega/petproject1/service/RoleService.java
+++ src/main/java/kg/mega/petproject1/service/RoleService.java
@@ -3,18 +3,13 @@ package kg.mega.petproject1.service;
 import kg.mega.petproject1.entity.Role;
 import kg.mega.petproject1.repository.RoleRepository;
 import lombok.AllArgsConstructor;
-import org.hibernate.ObjectNotFoundException;
 import org.springframework.stereotype.Service;
 
-
-import javax.management.relation.RoleNotFoundException;
-import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 @AllArgsConstructor
-
 public class RoleService {
     private final RoleRepository roleRepository;
 
@@ -30,11 +25,13 @@ public class RoleService {
         roleRepository.delete(role);
     }
 
-    public Role getById(int id) throws FileNotFoundException {
+    public Role getById(int id) {
         Optional<Role> role = roleRepository.findById((long) id);
         if (role.isPresent()) {
             return role.get();
-        } else throw new FileNotFoundException("role with this id not found in database");
+        } else {
+            throw new RuntimeException("Role with id: " + id + " not found");
+        }
     }
     //исправление
 }
