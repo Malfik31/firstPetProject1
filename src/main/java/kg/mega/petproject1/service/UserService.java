407d69d6808fa2f11145e2e06c5b07199655c969
User <mathmatchingacc@gmail.com>
2024-09-10 19:13:04 +0600

    UserService

---
+++ src/main/java/kg/mega/petproject1/service/UserService.java
@@ -0,0 +1,40 @@
+package kg.mega.petproject1.service;
+
+import kg.mega.petproject1.entity.Role;
+import kg.mega.petproject1.entity.User;
+import kg.mega.petproject1.repository.RoleRepository;
+import kg.mega.petproject1.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
+    public void delete(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    public User update(Long id, User user) {
+        if (userRepository.existsById(id)) {
+            user.setId(id);
+            return userRepository.save(user);
+        }
+        return null;
+    }
+}
