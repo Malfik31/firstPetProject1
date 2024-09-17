e68641ecf8c35e61e48a5d8870dc8b20b762c738
nurdas00 <nurdas00@gmail.com>
2024-09-17 18:47:13 +0600

    refresh project

--- src/main/java/kg/mega/petproject1/repository/GroupRepository.java
+++
@@ -1,9 +0,0 @@
-package kg.mega.petproject1.repository;
-
-import kg.mega.petproject1.entity.Group;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GroupRepository extends JpaRepository<Group, Long> {
-}
