54565141cc67e9090eb64d8a43f2d162687502aa
ttbbgg <168216078+ttbbgg@users.noreply.github.com>
2024-09-19 19:05:13 +0600

    create role entity (#33)
    * create role entity
    
    * create role entity

---
+++ src/main/java/kg/mega/petproject1/entity/Role.java
@@ -0,0 +1,17 @@
+package kg.mega.petproject1.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+@Entity
+@Getter
+@Setter
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+}
