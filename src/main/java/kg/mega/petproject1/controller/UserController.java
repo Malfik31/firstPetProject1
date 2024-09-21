162664d1b315b8769ca4129faceef7ff30eea0d1
Malfik31 <malfik31@gmail.com>
2024-09-21 18:30:16 +0600

    create userController (#35)
    * create userController
    
    * fix controller
    
    * fix controller
    
    * add method findById
    
    * all fix controller
    
    * fix all problems in controller
    
    * rename getUserById to getById

---
+++ src/main/java/kg/mega/petproject1/controller/UserController.java
@@ -0,0 +1,38 @@
+package kg.mega.petproject1.controller;
+
+import kg.mega.petproject1.entity.User;
+import kg.mega.petproject1.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping
+    public List<User> getAll() {
+        return userService.findAll();
+    }
+
+    @GetMapping("{/id}")
+    public void getById(@PathVariable Integer id) {
+        userService.getById(id);
+    }
+    @PostMapping
+    public void create(@RequestBody User user) {
+        userService.create(user);
+    }
+
+    @PutMapping
+    public void update(@RequestBody User user) {
+        userService.update(user);
+    }
+
+    @DeleteMapping("{/id}")
+    public void delete(@PathVariable Integer id) {
+        userService.deleteById(id);
+    }
+}
--- src/main/java/kg/mega/petproject1/service/UserService.java
+++ src/main/java/kg/mega/petproject1/service/UserService.java
@@ -1,11 +1,14 @@
 package kg.mega.petproject1.service;
 
+import jakarta.persistence.EntityNotFoundException;
 import kg.mega.petproject1.entity.User;
 import kg.mega.petproject1.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -27,4 +30,13 @@ public class UserService {
     public void create(User user) {
         userRepository.save(user);
     }
+
+    public void getById(@PathVariable Integer id) {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isPresent()) {
+            System.out.println(user.get());
+        }
+        throw new EntityNotFoundException("User with id: " + id + " not found");
+    }
+
 }
